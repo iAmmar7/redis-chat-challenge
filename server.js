d8e2e93c9a3bea84c1e942faa74403158534c4c5
Ammar Ansari <iammaransari@gmail.com>
2021-09-19 01:51:37 +0500

    few comments added on server

--- server.js
+++ server.js
@@ -10,18 +10,18 @@ const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-// Use Routes
+// API Routes
 app.use('/api', routes);
 
 http.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
 });
 
+// Start the socket
 socket(http);
